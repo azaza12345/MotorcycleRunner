efcfd3cf8cf2101f8f406d605e6a0aa2d2a2a0e3
Lev Logvinov <llogvinov356@gmail.com>
2021-03-30 22:21:51 +0300

    Add PlayerController script and new player image

--- MotorcycleRunner/Assets/EnemyController.cs
+++ MotorcycleRunner/Assets/EnemyController.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        
+
     }
 }
--- MotorcycleRunner/Assets/Scripts/PlayerController.cs
+++ MotorcycleRunner/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D myRigidbody;
-    // Start is called before the first frame update
+    private float topBound = 0.5f;
+    private float bottomBound = -4.5f;
+    private float leftBound = -9;
+    private float rightBound = 5;
+
+    private bool gameOver = false;
+
+    [SerializeField] private float speed = 10;
+    [SerializeField] private int health = 100;
+
     void Start()
     {
-        myRigidbody = GetComponent<Rigidbody2D>();
+        
     }
+
+    void Update()
+    {
+        ConstrainPlayerPosition();
+        MovePlayer();
+    }
+
+    // Moves player based on arrow key input (soon will make joystick)
+    void MovePlayer() {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(horizontalInput * speed * Time.deltaTime, verticalInput * speed * Time.deltaTime, 0);
+    }
+
+    // Prevents player to move out of the bounds
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.y > topBound) { transform.position = new Vector2(transform.position.x, topBound); }
+        if (transform.position.y < bottomBound) { transform.position = new Vector2(transform.position.x, bottomBound); }
+        if (transform.position.x > rightBound) { transform.position = new Vector2(rightBound, transform.position.y); }
+        if (transform.position.x < leftBound) { transform.position = new Vector2(leftBound, transform.position.y); }
+    }
+
 }
