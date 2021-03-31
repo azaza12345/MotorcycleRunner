499add1658ac9777488782d867305834010304f4
azaza12345 <61620351+azaza12345@users.noreply.github.com>
2021-03-31 21:51:22 +0300

    Remade player moving

--- MotorcycleRunner/Assets/Scripts/PlayerController.cs
+++ MotorcycleRunner/Assets/Scripts/PlayerController.cs
@@ -4,42 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float topBound = 0.5f;
-    private float bottomBound = -4.5f;
-    private float leftBound = -9;
-    private float rightBound = 5;
-
-    private bool gameOver = false;
-
     [SerializeField] private float speed = 10;
     [SerializeField] private int health = 100;
+    private Rigidbody2D myRigidBody;
+    private Vector2 move;
 
-    void Start()
+    private void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        ConstrainPlayerPosition();
         MovePlayer();
     }
 
     // Moves player based on arrow key input (soon will make joystick)
-    void MovePlayer() {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        transform.Translate(horizontalInput * speed * Time.deltaTime, verticalInput * speed * Time.deltaTime, 0);
-    }
-
-    // Prevents player to move out of the bounds
-    void ConstrainPlayerPosition()
+    private void MovePlayer()
     {
-        if (transform.position.y > topBound) { transform.position = new Vector2(transform.position.x, topBound); }
-        if (transform.position.y < bottomBound) { transform.position = new Vector2(transform.position.x, bottomBound); }
-        if (transform.position.x > rightBound) { transform.position = new Vector2(rightBound, transform.position.y); }
-        if (transform.position.x < leftBound) { transform.position = new Vector2(leftBound, transform.position.y); }
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+
+        myRigidBody.velocity = move * speed;
     }
 
 }
