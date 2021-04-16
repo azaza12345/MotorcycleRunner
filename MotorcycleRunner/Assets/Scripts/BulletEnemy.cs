dd3f6d3b6d9bdcba788316766fb55199980447ac
Lev Logvinov <llogvinov356@gmail.com>
2021-04-16 13:29:40 +0300

    make enemies shoot

---
+++ MotorcycleRunner/Assets/Scripts/BulletEnemy.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletEnemy : MonoBehaviour
+{
+    [SerializeField] private float bulletSpeed = 10;
+    [SerializeField] private float bound = 10;
+    [SerializeField] private int bulletDamage = 10;
+
+    private GameManager gameManager;
+    private Rigidbody2D bulletRb;
+    private PlayerHealth player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerHealth>();
+        bulletRb = GetComponent<Rigidbody2D>();
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void Update()
+    {
+        MoveBullet();
+        DestroyOutOfBounds();
+    }
+
+    // Makes bullet fly straight right
+    private void MoveBullet()
+    {
+        bulletRb.velocity = Vector2.left * bulletSpeed;
+    }
+
+    // If bullet is out of bounds, destroys it
+    private void DestroyOutOfBounds()
+    {
+        if (transform.position.x < -bound | transform.position.x > bound)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            Destroy(gameObject);
+            PlayerHealth player = other.gameObject.GetComponent<PlayerHealth>();
+            if (player)
+            {
+                player.TakeDamage(bulletDamage);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
